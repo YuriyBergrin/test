1d2fe7c171a34cc2776090cd06066f0dfd158d19
Bergrin Yuriy <yubergrin@fil-it.ru>
2020-04-10 20:08:32 +0300

    parallel runs

--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -3,7 +3,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertTrue;
@@ -15,28 +19,21 @@ public class SignUpTest {
     private static SignUpPage page;
 
     @Before
-    public void setUp() {
-        System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver");
-        if (driver != null) {
-            return;
-        }
-        driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            driver.quit();
-            driver = null;
-        }));
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities caps = DesiredCapabilities.firefox();
+        driver = new RemoteWebDriver(new URL("http://172.18.0.1:4444/wd/hub"), caps);
+        driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
     }
 
     @After
     public void tearDown() {
-//        driver.quit();
-//         driver = null;
+        driver.quit();
+        driver = null;
     }
 
     @Test
     public void typeInvalidYear() {
+        System.out.println("FIRST TEST");
         driver.get("https://www.spotify.com/us/signup/");
         page = new SignUpPage(driver);
         page.typeDay("1")
@@ -45,10 +42,12 @@ public class SignUpTest {
                 .setShare(true);
         assertTrue(page.isErrorVisible("Please enter a valid year."));
         assertFalse(page.isErrorVisible("When were you born?"));
+        System.out.println("FIRST TEST");
     }
 
     @Test
     public void typeInvalidEmail() {
+        System.out.println("SECOND TEST");
         driver.get("https://www.spotify.com/us/signup/");
         page = new SignUpPage(driver);
         page.typeEmail("test112324@test.com")
@@ -56,10 +55,12 @@ public class SignUpTest {
                 .typeName("yuriy")
                 .clickSignUpButton();
         assertTrue(page.isErrorVisible("Email address doesn't match."));
+        System.out.println("SECOND TEST");
     }
 
     @Test
     public void signUpWithEmptyPassword() {
+        System.out.println("THIRD TEST");
         driver.get("https://www.spotify.com/us/signup/");
         page = new SignUpPage(driver);
         page.typeEmail("test77712@test.ru")
@@ -67,10 +68,12 @@ public class SignUpTest {
                 .typeName("Vasya")
                 .clickSignUpButton();
         assertTrue(page.isErrorVisible("Enter a password to continue."));
+        System.out.println("THIRD TEST");
     }
 
     @Test
     public void typeInvalidValues() {
+        System.out.println("FOURTH TEST");
         driver.get("https://www.spotify.com/us/signup/");
         page = new SignUpPage(driver);
         page.typeEmail("1124324")
@@ -80,5 +83,6 @@ public class SignUpTest {
                 .setShare(false)
                 .clickSignUpButton();
         assertEquals(7, page.getErrors().size());
+        System.out.println("FOURTH TEST");
     }
 }
